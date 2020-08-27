0d7b49c7562c29220530b6ec2161b623990df387
Rizwan Atewalla <rizwan_53@hotmail.com>
2020-08-27 17:51:06 +0100

    Updates

---
+++ precache-manifest.1a6744f92ac2fb0c3f5489bda5d3f296.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4f0dbf367730c771e78c872e5f9bfe1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6cb0809488149b4d50bb",
+    "url": "/portfolio/static/js/2.f5366d8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.f5366d8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0d5ecd37eb0aedc29a9",
+    "url": "/portfolio/static/js/main.8979b177.chunk.js"
+  },
+  {
+    "revision": "f200766009ec73cc0acf",
+    "url": "/portfolio/static/js/runtime-main.ae584c50.js"
+  }
+]);
